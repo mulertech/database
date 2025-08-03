eff06403310e5e278548cc6ab223b168f8aea48e
Sébastien Muler <sebastien.muler@mulertech.net>
2025-08-03 19:09:27 +0200

    Refactor DependencyManager; add support for update and deletion dependencies, improve dependency management methods, and enhance circular dependency detection

--- src/ORM/State/DependencyManager.php
+++ src/ORM/State/DependencyManager.php
@@ -11,9 +11,19 @@ namespace MulerTech\Database\ORM\State;
 final class DependencyManager
 {
     /**
-     * @var array<int, array<int>> Insertion dependencies
+     * @var array<int, array<int, object>> Insertion dependencies
      */
     private array $insertionDependencies = [];
+
+    /**
+     * @var array<int, array<int, object>> Update dependencies
+     */
+    private array $updateDependencies = [];
+
+    /**
+     * @var array<int, array<int, object>> Deletion dependencies
+     */
+    private array $deletionDependencies = [];
 
     /**
      * @param object $dependent
@@ -25,7 +35,120 @@ final class DependencyManager
         $dependentId = spl_object_id($dependent);
         $dependencyId = spl_object_id($dependency);
 
-        $this->insertionDependencies[$dependentId][] = $dependencyId;
+        if (!isset($this->insertionDependencies[$dependentId][$dependencyId])) {
+            $this->insertionDependencies[$dependentId][$dependencyId] = $dependency;
+        }
+    }
+
+    /**
+     * @param object $dependent
+     * @param object $dependency
+     * @return void
+     */
+    public function addUpdateDependency(object $dependent, object $dependency): void
+    {
+        $dependentId = spl_object_id($dependent);
+        $dependencyId = spl_object_id($dependency);
+
+        if (!isset($this->updateDependencies[$dependentId][$dependencyId])) {
+            $this->updateDependencies[$dependentId][$dependencyId] = $dependency;
+        }
+    }
+
+    /**
+     * @param object $dependent
+     * @param object $dependency
+     * @return void
+     */
+    public function addDeletionDependency(object $dependent, object $dependency): void
+    {
+        $dependentId = spl_object_id($dependent);
+        $dependencyId = spl_object_id($dependency);
+
+        if (!isset($this->deletionDependencies[$dependentId][$dependencyId])) {
+            $this->deletionDependencies[$dependentId][$dependencyId] = $dependency;
+        }
+    }
+
+    /**
+     * @param object $entity
+     * @return array<int, object>
+     */
+    public function getInsertionDependencies(object $entity): array
+    {
+        $oid = spl_object_id($entity);
+        return $this->insertionDependencies[$oid] ?? [];
+    }
+
+    /**
+     * @param object $entity
+     * @return array<int, object>
+     */
+    public function getUpdateDependencies(object $entity): array
+    {
+        $oid = spl_object_id($entity);
+        return $this->updateDependencies[$oid] ?? [];
+    }
+
+    /**
+     * @param object $entity
+     * @return array<int, object>
+     */
+    public function getDeletionDependencies(object $entity): array
+    {
+        $oid = spl_object_id($entity);
+        return $this->deletionDependencies[$oid] ?? [];
+    }
+
+    /**
+     * @param object $dependent
+     * @param object $dependency
+     * @return void
+     */
+    public function removeInsertionDependency(object $dependent, object $dependency): void
+    {
+        $dependentId = spl_object_id($dependent);
+        $dependencyId = spl_object_id($dependency);
+
+        unset($this->insertionDependencies[$dependentId][$dependencyId]);
+
+        if (empty($this->insertionDependencies[$dependentId])) {
+            unset($this->insertionDependencies[$dependentId]);
+        }
+    }
+
+    /**
+     * @param object $dependent
+     * @param object $dependency
+     * @return void
+     */
+    public function removeUpdateDependency(object $dependent, object $dependency): void
+    {
+        $dependentId = spl_object_id($dependent);
+        $dependencyId = spl_object_id($dependency);
+
+        unset($this->updateDependencies[$dependentId][$dependencyId]);
+
+        if (empty($this->updateDependencies[$dependentId])) {
+            unset($this->updateDependencies[$dependentId]);
+        }
+    }
+
+    /**
+     * @param object $dependent
+     * @param object $dependency
+     * @return void
+     */
+    public function removeDeletionDependency(object $dependent, object $dependency): void
+    {
+        $dependentId = spl_object_id($dependent);
+        $dependencyId = spl_object_id($dependency);
+
+        unset($this->deletionDependencies[$dependentId][$dependencyId]);
+
+        if (empty($this->deletionDependencies[$dependentId])) {
+            unset($this->deletionDependencies[$dependentId]);
+        }
     }
 
     /**
@@ -36,6 +159,29 @@ final class DependencyManager
     {
         $oid = spl_object_id($entity);
         unset($this->insertionDependencies[$oid]);
+        unset($this->updateDependencies[$oid]);
+        unset($this->deletionDependencies[$oid]);
+    }
+
+    /**
+     * @param object $entity
+     * @return void
+     */
+    public function clearDependencies(object $entity): void
+    {
+        $this->removeDependencies($entity);
+    }
+
+    /**
+     * @param object $entity
+     * @return bool
+     */
+    public function hasDependencies(object $entity): bool
+    {
+        $oid = spl_object_id($entity);
+        return !empty($this->insertionDependencies[$oid]) ||
+               !empty($this->updateDependencies[$oid]) ||
+               !empty($this->deletionDependencies[$oid]);
     }
 
     /**
@@ -44,11 +190,14 @@ final class DependencyManager
     public function clear(): void
     {
         $this->insertionDependencies = [];
+        $this->updateDependencies = [];
+        $this->deletionDependencies = [];
     }
 
     /**
      * @param array<int, object> $entities
      * @return array<int, object>
+     * @throws \RuntimeException
      */
     public function orderByDependencies(array $entities): array
     {
@@ -58,9 +207,12 @@ final class DependencyManager
 
         $ordered = [];
         $visited = [];
+        $visiting = [];
 
         foreach ($entities as $oid => $entity) {
-            $this->visitEntity($oid, $entities, $visited, $ordered);
+            if (!isset($visited[$oid])) {
+                $this->visitEntity($oid, $entities, $visited, $visiting, $ordered);
+            }
         }
 
         return $ordered;
@@ -70,18 +222,35 @@ final class DependencyManager
      * @param int $oid
      * @param array<int, object> $entities
      * @param array<int, bool> $visited
+     * @param array<int, bool> $visiting
      * @param array<int, object> $ordered
+     * @throws \RuntimeException
      */
-    private function visitEntity(int $oid, array $entities, array &$visited, array &$ordered): void
-    {
+    private function visitEntity(
+        int $oid,
+        array $entities,
+        array &$visited,
+        array &$visiting,
+        array &$ordered
+    ): void {
         if (!isset($entities[$oid])) {
             return;
         }
 
+        if (isset($visiting[$oid])) {
+            throw new \RuntimeException('Circular dependency detected');
+        }
+
+        if (isset($visited[$oid])) {
+            return;
+        }
+
+        $visiting[$oid] = true;
+
+        $this->visitEntityDependencies($oid, $entities, $visited, $visiting, $ordered);
+
+        unset($visiting[$oid]);
         $visited[$oid] = true;
-
-        $this->visitEntityDependencies($oid, $entities, $visited, $ordered);
-
         $ordered[$oid] = $entities[$oid];
     }
 
@@ -89,14 +258,21 @@ final class DependencyManager
      * @param int $oid
      * @param array<int, object> $entities
      * @param array<int, bool> $visited
+     * @param array<int, bool> $visiting
      * @param array<int, object> $ordered
+     * @throws \RuntimeException
      */
-    private function visitEntityDependencies(int $oid, array $entities, array &$visited, array &$ordered): void
-    {
+    private function visitEntityDependencies(
+        int $oid,
+        array $entities,
+        array &$visited,
+        array &$visiting,
+        array &$ordered
+    ): void {
         $dependencies = $this->insertionDependencies[$oid] ?? [];
 
-        foreach ($dependencies as $dependencyId) {
-            $this->visitEntity($dependencyId, $entities, $visited, $ordered);
+        foreach ($dependencies as $dependencyId => $dependency) {
+            $this->visitEntity($dependencyId, $entities, $visited, $visiting, $ordered);
         }
     }
 }
