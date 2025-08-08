f33900eb24c83d049bec218e433ac1c013b2763c
Sébastien Muler <sebastien.muler@mulertech.net>
2025-08-08 13:46:48 +0200

    Add tests for FlushHandler; verify flush depth increments, process deletions, and reset functionality

--- tests/ORM/Engine/Persistence/FlushHandlerTest.php
+++ tests/ORM/Engine/Persistence/FlushHandlerTest.php
@@ -13,6 +13,7 @@ use MulerTech\Database\ORM\IdentityMap;
 use MulerTech\Database\ORM\State\StateManagerInterface;
 use MulerTech\Database\Tests\Files\Entity\User;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class FlushHandlerTest extends TestCase
 {
@@ -92,6 +93,76 @@ class FlushHandlerTest extends TestCase
         
         $this->flushHandler->doFlush($insertionProcessor, $updateProcessor, $deletionProcessor);
         
+        $this->assertTrue(true);
+    }
+
+    public function testGetFlushDepthIncrementsOnFlush(): void
+    {
+        // Initial depth should be 0
+        $this->assertEquals(0, $this->flushHandler->getFlushDepth());
+        
+        // After a flush, depth should be 1 (it's not decremented by finalizeFlush)
+        $this->flushHandler->doFlush(
+            function($entity) { /* dummy */ },
+            function($entity) { /* dummy */ },
+            function($entity) { /* dummy */ }
+        );
+        
+        $this->assertEquals(1, $this->flushHandler->getFlushDepth());
+    }
+
+    public function testProcessAdditionalOperationsWithNewDeletions(): void
+    {
+        $processedEntities = [];
+        $insertionProcessor = function($entity) use (&$processedEntities) {
+            $processedEntities[] = 'insert:' . get_class($entity);
+        };
+        $updateProcessor = function($entity) use (&$processedEntities) {
+            $processedEntities[] = 'update:' . get_class($entity);
+        };
+        $deletionProcessor = function($entity) use (&$processedEntities) {
+            $processedEntities[] = 'delete:' . get_class($entity);
+        };
+        
+        // Create entities for testing
+        $user1 = new User();
+        $user2 = new User();
+        
+        // Mock the state manager to return scheduled deletions
+        $this->stateManager->method('getScheduledDeletions')
+            ->willReturn([$user1, $user2]);
+        $this->stateManager->method('getScheduledInsertions')
+            ->willReturn([]);
+        
+        $this->flushHandler->doFlush($insertionProcessor, $updateProcessor, $deletionProcessor);
+        
+        // Verify that deletion operations were processed
+        $this->assertContains('delete:' . User::class, $processedEntities);
+    }
+
+    public function testResetResetsFlushDepthAndPostEventChanges(): void
+    {
+        $this->flushHandler->markPostEventChanges();
+        
+        // Do a flush to increase depth
+        $this->flushHandler->doFlush(
+            function($entity) { /* dummy */ },
+            function($entity) { /* dummy */ },
+            function($entity) { /* dummy */ }
+        );
+        
+        $this->assertGreaterThan(0, $this->flushHandler->getFlushDepth());
+        
+        $this->flushHandler->reset();
+        
+        $this->assertEquals(0, $this->flushHandler->getFlushDepth());
+    }
+
+    public function testMarkPostEventChanges(): void
+    {
+        $this->flushHandler->markPostEventChanges();
+        
+        // Test passes if no exception is thrown
         $this->assertTrue(true);
     }
 }
